47e5b6e5a38a846dd16cb24687ab2b7a6a4487e5
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-04-09 20:03:25 +0200

    Update SinglyLinkedList.java

--- SingleLinkedList/SinglyLinkedList.java
+++ SingleLinkedList/SinglyLinkedList.java
@@ -8,16 +8,33 @@
             this.next = null;
         }
     }
+    //Method for printing list elements
     void printList(){
         ListNode temp = head;
-        if(head == null){
-            System.out.println(0);;
-        }
         while(temp != null){
             System.out.print(temp.data+"--> ");
             temp = temp.next;
         }
         System.out.print("null");
+    }
+    //Method for getting length of the list
+    int length(){
+        ListNode temp = head;
+        int count = 0;
+        if(head == null){
+            return 0;
+        }
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+  //method when you add element at the start of the list
+    void addFirst(int val){
+        ListNode newNode = new ListNode(val);
+        newNode.next = head;
+        head = newNode;
     }
     public static void main(String[] args) {
     SinglyLinkedList sll = new SinglyLinkedList();
@@ -28,6 +45,9 @@
     sll.head.next = second;
     second.next = third;
     third.next = fourth;
+    sll.printList();
+    System.out.println("\n"+"List length: "+sll.length());
+    sll.addFirst(6);
     sll.printList();
     }
 }
